8d4d4f8d766a124816e9d31b97ae858f81e45656
Rafael Rocha <faelrocha07@gmail.com>
2014-06-24 22:31:48 -0400

    Filter on contact list

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,23 @@
 
 angular.module('myApp.controllers', [])
   .controller('List', ['$scope', function($scope) {
+  	$scope.contacts = [{
+  			id: 0,
+  			name: 'Arlindo',
+  			number: '35 223 4444',
+  			address: '34 Marcy Avenue - NY'
+  		},{
+  			id: 2,
+  			name: 'Rafael',
+  			number: '39 223 6666',
+  			address: '39 Brasil Avenue - NY'
+  		},{
+  			id: 3,
+  			name: 'Rhahsd',
+  			number: '37 223 6666',
+  			address: '39 Pernambuco Avenue - NY'
+  		}
+  	]
 
   }])
   .controller('Form', ['$scope', function($scope) {
